240832c28674b3f072567536d27877cbd801f168
Christopher Peel <175068649+cpeel42@users.noreply.github.com>
2024-07-23 22:26:27 -0400

    Add rounds, input validation and finish project

--- scripts.js
+++ scripts.js
@@ -9,5 +9,51 @@ function getRandomNumber(max) {
     return Math.floor(Math.random() * max);
 }
 
-// Logs the random choice to console
-console.log(getComputerChoice());
+// Prompt user for text and return value in lower case
+function getHumanChoice() {
+    let choice;
+    while(choice != "rock" && choice != "scissors" && choice != "paper") {
+        choice = prompt('Choose "rock", "paper" or "scissors".').toLowerCase();
+    }
+    return choice;
+}
+
+// Game logic to determine winner and increment scores
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    if (humanChoice == computerChoice) {
+        console.log("Tie! Both you and the computer chose " + computerChoice + ". " + getScores());
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors' || humanChoice == 'scissors' && computerChoice == 'paper' || humanChoice == 'paper' && computerChoice == 'rock') {
+        humanScore++;
+        console.log("You win! You chose " + humanChoice + " which beats " + computerChoice + ". " + getScores());
+    } else {
+        computerScore++;
+        console.log("You lose. The computer chose " + computerChoice + " which beats " + humanChoice + ". " + getScores());
+    }
+}
+
+function getScores() {
+    let message = "Score " + humanScore + ":" + computerScore;
+    return message;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+
+// Play 5 rounds
+while (round < 5) {
+    playRound();
+    ++round;
+}
+
+// Display the winner
+if (humanScore > computerScore) {
+    console.log("Congrats, you won!");
+} else if (computerScore > humanScore) {
+    console.log("Sorry, try again.");
+} else {
+    console.log("Tie game!");
+}
